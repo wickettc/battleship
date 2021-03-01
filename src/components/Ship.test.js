0fa113a047dd09803edf9e09c6f2915c1da5aef0
Chase Wickett <wickettc@yahoo.com>
2021-03-01 16:51:21 -0500

    init commit

---
+++ src/components/Ship.test.js
@@ -0,0 +1,31 @@
+import Ship from './Ship';
+
+let firstShip = Ship(5);
+
+test('create ship with length of 5', () => {
+    expect(firstShip.length).toBe(5);
+});
+
+firstShip.hit(0);
+
+// test('ship hit function', () => {
+//     expect(firstShip.hitLocations).toEqual([
+//         'X',
+//         undefined,
+//         undefined,
+//         undefined,
+//         undefined,
+//     ]);
+// });
+
+firstShip.hit(4);
+
+test('ship hit function again', () => {
+    expect(firstShip.hitLocations).toEqual([
+        'X',
+        undefined,
+        undefined,
+        undefined,
+        'X',
+    ]);
+});
